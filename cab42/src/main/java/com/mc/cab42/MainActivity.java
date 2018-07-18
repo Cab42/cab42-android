a31cea13b2ac1c25436ac26a5c886f7a29224f45
Mario Cuellar <mariocuellar1@gmail.com>
2018-07-18 21:40:25 +0100

    Initial commit.

---
+++ cab42/src/main/java/com/mc/cab42/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mc.cab42;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
